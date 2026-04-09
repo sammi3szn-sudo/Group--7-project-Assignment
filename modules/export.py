10cc88a19207592372267fa342ac6644589ea253
Jennifer <bignifer25@gmail.com>
2026-04-09 21:59:08 +0100

    Implement CSV and Excel export functions in export module

--- modules/export.py
+++ modules/export.py
@@ -1,9 +1,18 @@
-import sqlite3
 import pandas as pd
+import sqlite3
 
-def export_to_csv(filename="inventory_export.csv"):
+def export_to_csv():
     conn = sqlite3.connect("inventory.db")
     df = pd.read_sql_query("SELECT * FROM products", conn)
+    filename = "inventory.csv"
     df.to_csv(filename, index=False)
+    conn.close()
+    return filename
+
+def export_to_excel():
+    conn = sqlite3.connect("inventory.db")
+    df = pd.read_sql_query("SELECT * FROM products", conn)
+    filename = "inventory.xlsx"
+    df.to_excel(filename, index=False)
     conn.close()
     return filename
